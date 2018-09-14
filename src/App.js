223d3048ff1a5cef2f63040b60e739751b1cfb1c
Carlo Carpio <carlo@omh.sg>
2018-09-14 09:02:52 +0800

    Initial Commit for the sample components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Test from './components/test'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        asasasas
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Test  = () => {
+  return (
+    <div>this is a test</div>
+  )
+}
+
+Test.propTypes = {}
+
+export default Test
+
